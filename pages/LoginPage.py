a1870d50846c936d510a92eea943155458ed080e
Elya-autho <filippova.elvina2@gmail.com>
2025-06-28 16:32:28 +0300

    Update LoginPage.py
    Добавлен локатор для вкладки Вход

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -3,18 +3,17 @@ from selenium.webdriver.common.by import By
 
 
 class LoginPageLocators:
+    LOGIN_TAB = (By.XPATH, '//*[@data-l="t,login_tab"]')
+    QR_TAB = (By.XPATH, '//*[@data-l="t,qr_tab"]')
     LOGIN_FIELD = (By.ID,'field_email')
     PASSWORD_FIELD = (By.ID, 'field_password')
     LOGIN_BUTTON = (By.XPATH, '//*[@data-l="t,sign_in"]')
     QR_BUTTON = (By.XPATH, '//*[@id="qrCode-3407364914"]/span')
     FORGOT_PASSWORD = (By.XPATH, '//*[@tsid="restore"]')
-    REGISTER_BUTTON = (By.XPATH,'//div[@class="external-oauth-login-footer"]/a[@data-l="t,register"] ')
-
+    REGISTER_BUTTON = (By.XPATH,'//div[@class="external-oauth-login-footer"]/a[@data-l="t,register"]')
     VK_IKON = (By.XPATH, '//*[@data-module="registration/vkconnect"]')
     MAIL_IKON =(By.XPATH, '//*[@data-provider="MAILRU"]')
     YANDEX_IKON = (By.XPATH,'//*[@data-l="t,yandex"]')
-    QR_TAB = (By.XPATH,'//*[@data-l="t,qr_tab"]')
-
-
+   
 class LoginPageHelper(BasePage):
     pass
